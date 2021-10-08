a9bdc4e0a661a70aa1c580ba1b9174942e8c2e67
lux <luischn94@gmail.com>
2021-10-07 23:50:12 -0300

    destroy btn

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -8,4 +8,9 @@ class PicsController < ApplicationController
   def pic_params
     params.require(:pic).permit(:title, :photo)
   end
+
+  def destroy_all_posts
+    Pic.destroy_all
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   resources :pics, only: :create
+  delete "destroy_all_posts", to: "pics#destroy_all_posts"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
